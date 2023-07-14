fabb7a30d96d835be40b2673394a14d192e5c52c
LUCAS MONTEIRO RIBEIRO <lucas.monteiro58@gmail.com>
2023-07-14 15:03:59 -0300

    feat: ✨add plugins

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="w-screen h-screen bg-slate-700"></main>
+  <main
+    class="w-screen h-screen bg-slate-700 flex items-center justify-center text-slate-50 font-black text-5xl"
+  >
+    <span>Vue 3 + Typescript + Tailwind Boilerplate</span>
+  </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,42 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      dts: 'src/components.d.ts',
+      include: [/\.vue$/, /\.vue\?vue/]
+    }),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'vue/macros', '@vueuse/head', '@vueuse/core'],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/components', 'src/composables', 'src/utils', 'src/stores'],
+      vueTemplate: true,
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true
+      }
+    }),
+    Unfonts({
+      google: {
+        families: [
+          {
+            name: 'Roboto',
+            styles: 'wght@300;400;500;700;900'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
